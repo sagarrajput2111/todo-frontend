03a6259cf25a0bc5c12ffe6de666897d96f52a27
Sagar Singh <sagarrajput2111@gmail.com>
2024-06-17 14:05:30 +0530

    updated Ip commit

--- src/app/home/api.service.ts
+++ src/app/home/api.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ApiService {
-  private apiUrl = 'http://localhost:3000'; // Example API URL
+  private apiUrl = 'http://20.83.184.163:3000'; // Example API URL
 
   constructor(private http: HttpClient) { }
   // callApi():Observable<any[]>{
